e4f85eecab0c72362e5c24bf95a5213c214dff66
Malek <malekgeshash@gmail.com>
2022-10-18 02:20:56 +0100

    add basic modal popup when buttons are clicked

--- house-swiper/components/Nav.js
+++ house-swiper/components/Nav.js
@@ -1,10 +1,16 @@
 import styles from '../styles/Home.module.css'
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 export  default function Nav(){
     return(
     <nav className={styles.navstyle}>
-        <button className= {styles.buttons} >Create account</button>
-        <button className= {styles.buttons}>Log in</button>
+        <Popup trigger={<button className= {styles.buttons} >Create account</button>}  modal="true">
+            <div>Create account popup</div>
+        </Popup>
+        <Popup trigger={<button className= {styles.buttons}>Log in</button>}  modal="true">
+            <div>Log  in popup</div>
+        </Popup>
       </nav>
     )
 }
