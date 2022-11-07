9a7e6e68049543756b27354e430db77c07035652
MALEK GESHASH - STUDENT <G00387032@atu.ie>
2022-11-07 16:07:59 +0000

    work in progress

--- house-swiper/components/editProfile/ProfileDetails.js
+++ house-swiper/components/editProfile/ProfileDetails.js
@@ -1,10 +1,18 @@
 import styles from "./ProfileDetails.module.css"
+import Image from 'next/image'
 
 export default function ProfileDetails(props){
     return(
         <div className = {styles.container}>
-        <name>Malek</name>
-        <age>23</age>
+            <div>
+                <p>Image: {props.img}</p>
+                <Image src = "/houseSwiperHomePagePic.JPG" width = "500px" height= "500px"></Image>
+                <button>edit image</button>
+            </div>
+            <div>
+                <p>Description: {props.description}</p>
+                <button>edit description</button>
+            </div>
         </div>
     )
 
