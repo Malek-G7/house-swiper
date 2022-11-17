e03b98a4967105a39f9c922e73f331e59adc8e50
MALEK GESHASH - STUDENT <G00387032@atu.ie>
2022-11-17 12:58:44 +0000

    work in progress - addingExpress

--- house-swiper/pages/mainpage.js
+++ house-swiper/pages/mainpage.js
@@ -1,11 +1,24 @@
 
 import Nav from '../components/mainpage/Nav'
 import Body from '../components/mainpage/Body'
-export default function MainPage() {
+export default function MainPage({data}) {
   return (
     <div >
       <Nav />
-      <Body />
+      <Body data = {{data}}/>
     </div>
   )
+}
+export async function getServerSideProps() {
+  // Fetch data from external API
+
+  const res = await fetch(`http://localhost:5000/`)
+  const data = await res.json()
+  console.log("data is " + data)
+
+
+  // Pass data to the page via props
+  return {
+     props:  {data}
+    }
 }
