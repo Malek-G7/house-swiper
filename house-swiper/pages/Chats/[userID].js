5edfb09c54552548bd5091865740cc9d6eb07187
MALEK GESHASH - STUDENT <G00387032@atu.ie>
2023-02-14 17:39:35 +0000

    styling

--- house-swiper/pages/Chats/[userID].js
+++ house-swiper/pages/Chats/[userID].js
@@ -2,6 +2,8 @@ import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import axios from "axios"
 import styles from "./chats.module.css"
+import Nav from "/components/matches/Nav.js"
+
 export default function ChatPage() {
     const router = useRouter();
     const userID = router.query.userID
@@ -13,13 +15,15 @@ export default function ChatPage() {
 
     function settingMessages(data) {
         return (
+            <div className= {styles.messagesContainer}>
             <ul className={styles.list}>
                 {data.map((messageContent, index) => (
                     <li>
-                        <div>{messageContent.from} : {messageContent.message}</div>
+                        <div className={styles.messages}>{messageContent.from} : {messageContent.message}</div>
                         <div></div>
                     </li>))}
             </ul>
+            </div>          
         )
     }
     useEffect(() => {
@@ -36,27 +40,34 @@ export default function ChatPage() {
         fetchData()
         setInterval(() => {
             setFlag(prev => !prev)
-        }, 1000);
+        }, 5000);
         // setMesagges(settingMessages(texts))
     }, [])
 
     async function sendMessageHandler(event) {
         event.preventDefault()
-        const response = await axios.post("http://localhost:5000/messaging/sendChat", { talkingTo: userID, message: chatboxContent }, { withCredentials: true }, { headers: { 'Content-Type': 'application/json' } })
-        console.log(response)
+        if(chatboxContent.trim.length != 0){
+            const response = await axios.post("http://localhost:5000/messaging/sendChat", { talkingTo: userID, message: chatboxContent }, { withCredentials: true }, { headers: { 'Content-Type': 'application/json' } })
+            console.log(response)
+        }        
     }
     return (
-
-        <div>
-            <h1>you are talking to {userID}</h1>
-            <div>{messages}</div>
+        
+        <div className={styles.container}>
+            <Nav/>
             <div>
+            <h1>you are talking to {userID}</h1>
+            </div>
+            <div>
+                {messages}
+            </div>
+            <div className={styles.chatBox}>
                 <div id="message-container"></div>
                 <form id="send-container" onSubmit={sendMessageHandler}>
-                    <input type="text" id="message-input" value={chatboxContent} onChange={(newText) => {
+                    <input type="text" className={styles.inputBox} id="message-input" value={chatboxContent} onChange={(newText) => {
                         setChatBoxContent(newText.target.value)
                     }}></input>
-                    <button type="submit" id="send-button">Send</button>
+                    <button  className={styles.buttons} type="submit" id="send-button">Send</button>
                 </form>
             </div>
         </div>)
