6230b791b76d5839323dca5115c3938c399a8469
MALEK GESHASH <G00387032@atu.ie>
2023-04-13 15:37:37 +0100

    load env variables in next config

--- house-swiper/next.config.js
+++ house-swiper/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const { parsed: localEnv } = require('dotenv').config({
+  path: '.env.local',
+});
+
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    SERVER_URI: localEnv.SERVER_URI,
+  }
 }
 
 module.exports = nextConfig
